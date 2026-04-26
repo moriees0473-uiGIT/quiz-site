a9a8851dfabb817379a3e6d343105240aa5034e4
moriees25 <moriees0473@gmail.com>
2026-04-26 14:26:59 +0900

    1st

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@
 import React, { useState, useEffect } from 'react';
 import { useRouter } from 'next/navigation'; // 追加：ページ移動のための道具
 import { io, Socket } from 'socket.io-client';
-import { SystemStatus, UserRole } from '@/types/types';
+import { SystemStatus, UserRole } from '../types/types';
 
 const socket: Socket = io(); // 引数を空にすると、自分自身(localhost:3000)に繋ぎにいきます
 export default function Home() {
